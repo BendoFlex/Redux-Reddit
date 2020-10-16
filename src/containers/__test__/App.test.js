c78d25f80cd819fac611ff57c697788f4c54fd74
bendoflex <joanzaf@lilo.org>
2020-10-16 15:39:33 +0200

    first test of a snapshot

--- src/containers/__test__/App.test.js
+++ src/containers/__test__/App.test.js
@@ -1,1 +1,21 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import toJson from 'enzyme-to-json' 
+
 import {App} from '../App'
+
+
+describe ( 'App', () => {
+   
+    it('it renders without crashing', () => {
+        const props = {
+            isFetching = false,
+            dispatch = jest.fn(),
+            selectedSubreddit: 'reactjs',
+            posts: []
+        }
+        const wrapper = shallow(<App {...props}/>)
+        expect( toJson(wrapper)).toMatchSnapShot()
+    })
+
+}) // describe>it>expect? test de snapshots
